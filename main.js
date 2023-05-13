d8e18db43243727be8436c65bcfa76626eaeb3c1
SAS-ghosty <87755075+SAS-ghosty@users.noreply.github.com>
2023-05-13 10:25:18 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+function preload() {
+}
+
+function setup() {
+    canvas = createCanvas(300, 300);
+    canvas.centre();
+}
+
+function draw() {
+}
+
+function take_snapshot(){
+    save('myFilterImage.png')
+}
